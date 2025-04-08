b8ca0830ebe1960631a0952e06fb31c9339e2d64
Kiryl Makhveyenia <mahveenya.k.a@gmail.com>
2025-04-09 00:00:36 +0300

    feat: retry solution

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -130,8 +130,19 @@ function memoize(func) {
  * }, 2);
  * retryer() => 2
  */
-function retry(/* func, attempts */) {
-  throw new Error('Not implemented');
+function retry(func, attempts) {
+  return function retryFunc() {
+    let error;
+    for (let i = 0; i < attempts; i += 1) {
+      try {
+        const result = func();
+        return result;
+      } catch (e) {
+        error = e;
+      }
+    }
+    throw error;
+  };
 }
 
 /**
